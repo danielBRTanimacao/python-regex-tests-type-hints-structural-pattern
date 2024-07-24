b41144d37c4befaaa699cc392b575eb4eab6c868
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-24 12:30:11 -0300

    type anotations vendo o basico yt

--- type_hints/main.py
+++ type_hints/main.py
@@ -1,8 +1,11 @@
-uma_string: str = 'Uma string'
-um_inteiro: int = 123456789
-um_float: float = 1.234
-um_boolean: bool = True
-um_set: set = {1, 2, 3} # mais sobre a seguir
-uma_lista: list = [] # mais a seguir
-uma_tupla: tuple = 1, 2, 3 # mais a seguir
-um_dicionario: dict = {} # mais sobre a seguir
+# uma_string: str = 'Uma string'
+# um_inteiro: int = 123456789
+# um_float: float = 1.234
+# um_boolean: bool = True
+# um_set: set = {1, 2, 3} # mais sobre a seguir
+# uma_lista: list = [] # mais a seguir
+# uma_tupla: tuple = 1, 2, 3 # mais a seguir
+# um_dicionario: dict = {} # mais sobre a seguir
+
+def soma(x: int, y: int) -> float:
+    return x + y

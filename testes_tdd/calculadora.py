a66eb9c7ada1620aa38a4f4a47bb85d14ca3547c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-25 10:18:04 -0300

    usando doctest e assertions

--- testes_tdd/calculadora.py
+++ testes_tdd/calculadora.py
@@ -3,13 +3,28 @@ def soma(x: int | float, y: int | float) -> float:
         Soma x e y
     >>> soma('10', 20)
     30
+
+    >>> soma(10, -10)
+    0
+
+    >>> soma('10', 20)
+    Traceback (most recent call last):
+    ...
     """
     assert isinstance(x, (int, float)), 'x precisa ser int ou float'
     return x + y
     # utilizando assertions (principalmente usado para devs não para user final)
     # utilizando DocTest
 
+def subtra(x, y):
+    # Função que faz apenas uma coisa
+    """
+    >>> subtra(10, 10)
+    0
+    """
+    return x - y
+
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod()
+    doctest.testmod(verbose=True)
